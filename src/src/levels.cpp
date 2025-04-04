bf34301d1837a59c0f545c82cb50a5f0578f22d5
Aly <aly.kazani52@gmail.com>
2025-04-04 14:51:00 +0500

    Update Level 1 Design

---
+++ src/src/levels.cpp
@@ -0,0 +1,196 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "constants.cpp"
+
+struct SpriteBounds{
+    sf::Vector2f position; // position of the sprite
+    sf::Vector2f size = {TILE_SIZE, TILE_SIZE}; // size of the sprite
+};
+
+class Props{
+    private:
+        sf::Texture propTexture; // prop texture
+        int (*currentProps)[LEVEL_WIDTH]; // Pointer to the prop layout
+        int LevelNumber;
+    public:
+        Props() : LevelNumber(0) {
+            currentProps = PROPS[0];
+            PROPS[0][3][1] = 33;
+            PROPS[0][3][20] = -33;
+            PROPS[0][5][1] = 33;
+            PROPS[0][5][20] = -33;
+            PROPS[0][11][20] = -33;
+            PROPS[0][11][1] = 33;
+        }
+        void LoadProps(){
+            if (!propTexture.loadFromFile("../resources/Dungeon_16x16_asset_pack/tileset.png")) {
+                std::cout << "Props didn't load successfully!\n";
+            } 
+            else { 
+                std::cout << "Props loaded successfully!\n";
+            }
+            propTexture.setSmooth(false);
+
+            currentProps = PROPS[LevelNumber];
+        }
+        void Render(sf::RenderWindow& window){
+            sf::Sprite tempSprite(propTexture);
+
+            for (int y = 0; y < LEVEL_HEIGHT; ++y) {
+                for (int x = 0; x < LEVEL_WIDTH; ++x) {
+                    int proptile = abs(currentProps[y][x]);
+
+                    if(PROPS[LevelNumber][y][x] != 0){
+                        if(abs(PROPS[LevelNumber][y][x]) == 33){
+                            if(PROPS[LevelNumber][y][x] == 33){
+                                // calculate prop sprite used from tile set
+                                int proptileX = (proptile % maptilesetColumns) * TILE_SIZE;
+                                int proptileY = (proptile / maptilesetColumns) * TILE_SIZE;
+        
+                                sf::IntRect proptilerect({proptileX, proptileY}, {TILE_SIZE, TILE_SIZE});
+        
+                                tempSprite.setTextureRect(proptilerect);
+                                tempSprite.setScale({SCALE, SCALE});
+                                float posx = x * TILE_SIZE * SCALE + (TILE_SIZE * SCALE) / 3;
+                                float posy = y * TILE_SIZE * SCALE;
+                                tempSprite.setPosition(sf::Vector2f(posx, posy));
+                            } else{
+                                // calculate prop sprite used from tile set
+                                int proptileX = (proptile % maptilesetColumns) * TILE_SIZE;
+                                int proptileY = (proptile / maptilesetColumns) * TILE_SIZE;
+                                sf::IntRect proptilerect({proptileX, proptileY}, {TILE_SIZE, TILE_SIZE});
+                                tempSprite.setTextureRect(proptilerect);
+                                tempSprite.setScale({-SCALE, SCALE});
+                                float posx = (x + 1) * TILE_SIZE * SCALE - (TILE_SIZE * SCALE) / 3;
+                                float posy = y * TILE_SIZE * SCALE;
+                                tempSprite.setPosition(sf::Vector2f(posx, posy));
+
+                            }
+                        }
+                    } else{ continue; }
+
+                    window.draw(tempSprite);
+                }
+            }            
+        }
+        ~Props(){}
+};
+
+class Map {
+    private:
+        sf::Texture mapTexture; // map tile texture
+        int (*currentMap)[LEVEL_WIDTH]; // Pointer to the current map array
+        int LevelNumber; // Current level number
+
+        struct SpriteBounds mapsprites[LEVEL_WIDTH][LEVEL_HEIGHT];  // Array of map sprite bounds
+
+    public:
+        Map() : LevelNumber(0) {
+            currentMap = MAPS[0];
+        }
+
+        SpriteBounds getMapTileSprite(int index_X , int index_Y){ // returns the sprite of the map tile at the given index
+            return mapsprites[index_X][index_Y];  
+        };
+
+        void LoadMap(){
+            if (!mapTexture.loadFromFile("../resources/Dungeon_16x16_asset_pack/tileset.png")) {
+                std::cout << "Map didn't load successfully!\n";
+            } 
+            else { 
+                std::cout << "Map loaded successfully!\n";
+            }
+            mapTexture.setSmooth(false);
+
+            currentMap = MAPS[LevelNumber];
+        }
+
+        void Render(sf::RenderWindow& window){
+            sf::Sprite tempSprite(mapTexture);
+
+            for (int y = 0; y < LEVEL_HEIGHT; ++y) {
+                for (int x = 0; x < LEVEL_WIDTH; ++x) {
+                    int maptile = currentMap[y][x];
+
+                    int maptileX = (maptile % maptilesetColumns) * TILE_SIZE;
+                    int maptileY = (maptile / maptilesetColumns) * TILE_SIZE;
+
+                    sf::IntRect maptilerect({maptileX, maptileY}, {TILE_SIZE, TILE_SIZE});
+
+                    tempSprite.setTextureRect(maptilerect);
+                    tempSprite.setScale({SCALE, SCALE});
+                    tempSprite.setPosition(sf::Vector2f((float)(x * TILE_SIZE * SCALE), (float)(y * TILE_SIZE * SCALE)));
+
+                    mapsprites[x][y].position = tempSprite.getGlobalBounds().position;
+                    mapsprites[x][y].size = tempSprite.getGlobalBounds().size;
+
+                    window.draw(tempSprite);
+                }
+            }            
+        }
+
+        ~Map(){}
+};
+
+class Floor {
+    private:
+        sf::Texture floorTexture; // floor tile texture
+        const int (*currentFloor)[LEVEL_WIDTH]; // Pointer to the current floor array
+        int LevelNumber; // current level number
+
+    public:
+        Floor() : LevelNumber(0) {
+            currentFloor = FLOORS[0];
+        }
+
+        void LoadFloor(const int level) { //    takes the level number and loads the corresponding level
+            if (level >= 0 && level < NUMBER_OF_LEVELS) {
+                LevelNumber = level;
+                if ( LevelNumber == 0) {
+                    if (!floorTexture.loadFromFile("../resources/Mini_Dungeon_Tileset/FREE/1_MiniDungeon_Tileset_Background1.png")) {
+                        std::cout << "Texture didn't load successfully!\n";
+                    } 
+                    else { 
+                        std::cout << "Texture loaded successfully!\n"; 
+                    }
+                    floorTexture.setSmooth(false);
+
+                }
+                if ( LevelNumber == 1) {
+                    
+                    floorTexture.setSmooth(false);
+
+                }
+                if ( LevelNumber == 2) {
+
+                }
+                currentFloor = FLOORS[LevelNumber];
+            }
+        }
+
+        void Render(sf::RenderWindow& window) {
+
+            sf::Sprite floorSprite(floorTexture);
+
+            for (int y = 0; y < LEVEL_HEIGHT; ++y) {
+                for (int x = 0; x < LEVEL_WIDTH; ++x) {
+
+                    int floortile = currentFloor[y][x];
+
+                    // Calculate row and column in the tileset
+                    int floortileX = (floortile % floortilesetColumns) * TILE_SIZE;
+                    int floortileY = (floortile / floortilesetColumns) * TILE_SIZE;
+
+                    sf::IntRect floortilerect({floortileX, floortileY}, {TILE_SIZE, TILE_SIZE});
+
+                    floorSprite.setTextureRect(floortilerect);
+                    floorSprite.setScale({SCALE, SCALE});
+                    floorSprite.setPosition(sf::Vector2f((float)(x * TILE_SIZE * SCALE), (float)(y * TILE_SIZE * SCALE)));
+
+                    window.draw(floorSprite);
+                }
+            }
+        }
+        ~Floor(){}
+};
+
---
+++ src/src/main.cpp
@@ -0,0 +1,52 @@
+#include <SFML/Graphics.hpp>
+#include "levels.cpp"
+#include "constants.cpp"
+
+int main(){
+
+    // INITIALIZE GAME
+
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(LEVEL_WIDTH * TILE_SIZE * SCALE, LEVEL_HEIGHT * TILE_SIZE * SCALE)), "Final Standoff");
+    Floor floor;
+    Map map;
+    Props prop;
+    // Prop props;  
+    floor.LoadFloor(0);    // Game Starts at Level 0, then 1, then finally, 2
+    map.LoadMap();         // Walls
+    prop.LoadProps();       // Props
+
+    sf::Clock clock;
+    int framecounter = 0;
+    window.setFramerateLimit(60);  // Max FrameRate set to 60 
+
+    while (window.isOpen()){
+        // INITIALIZE (LOOP)
+        framecounter++; // count frames...
+
+
+        while (const std::optional event = window.pollEvent()){
+            if (event->is<sf::Event::Closed>()){        // if the user closes the window
+                window.close();
+            }
+        }
+        
+        // UPDATE
+        sf::Time timer = clock.restart();
+        float deltaTime = timer.asMilliseconds(); // get the time since last frame in milliseconds
+
+
+        // DRAW
+        window.clear();     // clearing the window each frame
+        floor.Render(window); // rendering the level
+        map.Render(window); // rendering the map
+        prop.Render(window); // rendering the props
+        // render characters here
+        
+
+        window.display();   // displaying the window (important)
+    }
+        // DEINITIALIZE GAME
+
+
+        return 0;
+}
